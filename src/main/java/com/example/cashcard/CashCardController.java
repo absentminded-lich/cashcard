7a0bb1154e9a2a7250bbd819e9fc8c93319e24ea
Brett Parker <absentminded.lich@gmail.com>
2023-03-06 22:25:03 -0500

    Refactor findById to return not found

--- src/main/java/com/example/cashcard/CashCardController.java
+++ src/main/java/com/example/cashcard/CashCardController.java
@@ -11,8 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class CashCardController {
 
     @GetMapping("/{id}")
-    public ResponseEntity<CashCard> findById(@PathVariable String id) {
-        CashCard cashCard = new CashCard(99L, 123.45);
-        return ResponseEntity.ok(cashCard);
+    public ResponseEntity<CashCard> findById(@PathVariable Long id) {
+        if (id.equals(99L)) {
+            CashCard cashCard = new CashCard(99L, 123.45);
+            return ResponseEntity.ok(cashCard);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
